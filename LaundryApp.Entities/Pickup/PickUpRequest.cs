ffe895894d13261361bca064fdf4b157cfc50a08
Sreehari Aranghat <sreehari.aranghat@outlook.com>
2024-02-28 12:45:40 +0530

    Added Pickup Request

---
+++ LaundryApp.Entities/Pickup/PickUpRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryApp.Entities
+{
+    public class PickUpRequest : BaseObject
+    {
+        [Required]
+        public Customer Customer { get; set; }
+
+        [Required]
+        public DateTime? PrefedTimeStart { get; set; }
+
+        [Required]
+        public DateTime? PrefedTimeEnd { get; set; }
+
+        [Required]
+        public PickupStatus Status { get; set; } = PickupStatus.Pending;
+
+        public Employee? AssignedTo {  get; set; }
+
+        public DateTime? PickUpTime {  get; set; }
+    }
+}
